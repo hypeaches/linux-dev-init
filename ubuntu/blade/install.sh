8cc3bd3985d52c917911e17b7f9048cee8265fdb
liuxing01 <liuxing01@sogou-inc.com>
2019-08-29 21:24:27 +0800

    更新blade安装路径

--- ubuntu/blade/install.sh
+++ ubuntu/blade/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-user_bin_src=$HOME/bin/src/
+user_bin_src=$HOME/.bin/src/
+user_bin=$HOME/.bin/bin
 
 if [ ! -d $user_bin_src ]
 then
@@ -10,8 +11,13 @@ cd $user_bin_src
 
 git clone https://github.com/chen3feng/blade-build.git
 cd blade-build
-./install
 sed -i 's/benchmark/# benchmark/' blade.conf
 sed -i 's/extra_incs/# extra_incs/' blade.conf
+
+if [ ! -d $user_bin ]
+then
+	mkdir -p $user_bin
+fi
+ln -s $user_bin_src/blade-build/blade $user_bin/blade
 
 apt-get install -y python scons
